be301bfeae381e93050c871692c8bef8dd33d083
wtz <1369130123@qq.com>
2018-12-05 22:32:46 +0800

    add role control

--- src/main/java/com/ucas/wtz/cap/Configure/WebSecurityConfig.java
+++ src/main/java/com/ucas/wtz/cap/Configure/WebSecurityConfig.java
@@ -25,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/uploadImg","/deleteImg").hasRole("ROLE_ADMIN")
                 .antMatchers("/images/**","/webjars/**", "/components/**","/js/**","/css/**","/templates/**").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
