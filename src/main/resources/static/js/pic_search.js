5be09668134e4bcb927993ddc5019f791020c14f
M954 <34397530+M954@users.noreply.github.com>
2019-02-28 09:53:08 +0800

    详细信息位置修正 (#4)

--- src/main/resources/static/js/pic_search.js
+++ src/main/resources/static/js/pic_search.js
@@ -1,9 +1,9 @@
 console.log("hhh");
-// var token = $("meta[name='_csrf']").attr("content");
-// var header = $("meta[name='_csrf_header']").attr("content");
-// $(document).ajaxSend(function(e, xhr, options) {
-//     xhr.setRequestHeader(header, token);
-// });
+var token = $("meta[name='_csrf']").attr("content");
+var header = $("meta[name='_csrf_header']").attr("content");
+$(document).ajaxSend(function(e, xhr, options) {
+    xhr.setRequestHeader(header, token);
+});
 
 init_picture();
 initRecord();
