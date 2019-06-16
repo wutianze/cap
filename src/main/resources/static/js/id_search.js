4dc5483fc3f3c9f93fd489b7f85f80414d2d8aa6
xuqinqi <xu770310@163.com>
2019-06-16 22:35:12 +0800

    单个图片搜索

--- src/main/resources/static/js/id_search.js
+++ src/main/resources/static/js/id_search.js
@@ -1,86 +1,82 @@
+var token = $("meta[name='_csrf']").attr("content");
+var header = $("meta[name='_csrf_header']").attr("content");
+$(document).ajaxSend(function(e, xhr, options) {
+    xhr.setRequestHeader(header, token);
+});
+
 $(".id_search").click(function(){
-    content = $(this).siblings('input');
+    content = $($(this).siblings('input')).val();
     let show = $('.picture_show');
+    console.log(content);
     show.removeAttr('hidden');
     $(this).removeClass('id_search_before');
     $(this).addClass('id_search_after');
     $.ajax({
-        url: '/image_detail',
+        url: '/search',
         type: 'POST',
-        cache: false,
-        data: {'id':content
+        data: {
+            'id': content,
+            'content':"",
+            'provider': "",
+            'place': "",
+            'starttime': "",
+            'endtime': ""
         },
-        processData: false,
-        contentType: false,
-        success:function(res){
+        success: function (res) {
             picture_detail(res);
             let show = $('.picture_show');
             show.removeAttr('hidden');
         }
-    }).done(function(res) {
-    }).fail(function(res) {
+    }).done(function (res) {
+    }).fail(function (res) {
     });
 });
 
+
+
 // "{\"id\":\"1\", \"provider\":\"哈哈哈\", \"label\":\"a\tb\", \"dateTime\":\"2018-05-12\", \"place\":\"雁栖湖\", \"description\":\"秋天拍的\", \"published\":\"0\", \"copyright\":\"0\", \"imgPath\":\"../images/1.png\"}"
 
+
+
 function picture_detail(res){
-    res = res.replace("\t","$");
+    res = res.replace("\\t","$");
+    res = eval('(' + res + ')');
+    res = res[0];
     console.log(res);
-    JSON.parse(res, function(k, v) {
-        console.log(k, v);
-        if(k == 'id'){
-            let detail_id = $(".detail_id");
-            detail_id.text(v);
-        }
-        if(k == 'provider'){
-            let detail_author = $('.detail_author');
-            detail_author.text(v);
-        }
-        if(k == 'label'){
-            let detail_label = $('.detail_label');
-            detail_label.empty();
-            let tags = v.split("$");
-            for(let i = 0; i < tags.length; i++){
-                console.log(tags[i]);
-                let tag = $("<a></a>").text(tags[i]);
-                tag.addClass("ui label");
-                detail_label.append(tag);
-            }
-        }
-        if(k == 'dateTime'){
-            let detail_date = $('.detail_date');
-            detail_date.text(v);
-        }
-        if(k == 'place'){
-            let detail_place = $('.detail_place');
-            detail_place.text(v);
-        }
-        if(k == 'description'){
-            let detail_description = $('.detail_description');
-            detail_description.text(v);
-        }
-        if(k == 'published'){
-            let detail_published = $('.detail_published');
-            if(v == '0'){
-                detail_published.text('已发布');
-            }else{
-                detail_published.text('未发布');
-            }
-        }
-        if(k == 'copyright'){
-            let detail_copyright = $('.detail_copyright');
-            if(v == '0'){
-                detail_copyright.text('有版权');
-            }else{
-                detail_copyright.text('无版权');
-            }
-        }
-        if(k == 'imgPath'){
-            let show_detail = $('.show_detail');
-            show_detail.attr("src", v);
-        }
-    });
+    let detail_id = $(".detail_id");
+    detail_id.text(res.id);
+    let detail_author = $('.detail_author');
+    detail_author.text(res.provider);
+    let detail_label = $('.detail_label');
+    detail_label.empty();
+    let tags = res.label.split("$");
+    for(let I = 0; I < tags.length-1; I++){
+        console.log(tags[I]);
+        let tag = $("<a></a>").text(tags[I]);
+        tag.addClass("ui label");
+        detail_label.append(tag);
+    }
+    let detail_date = $('.detail_date');
+    detail_date.text(res.dateTime);
+    let detail_place = $('.detail_place');
+    detail_place.text(res.place);
+    let detail_description = $('.detail_description');
+    detail_description.text(res.description);
+    let detail_published = $('.detail_published');
+    if(res.published == '0'){
+        detail_published.text('已发布');
+    }else{
+        detail_published.text('未发布');
+    }
+    let detail_copyright = $('.detail_copyright');
+    if(res.copyright == '0'){
+        detail_copyright.text('有版权');
+    }else{
+        detail_copyright.text('无版权');
+    }
+    let show_detail = $('.show_detail');
+    console.log(res.imgPath);
+    show_detail.attr("src", res.imgPath);
 }
 
 $('.delbutton').click(function(){
@@ -89,27 +85,27 @@ $('.delbutton').click(function(){
     swal({
             title: "确定删除吗？",
             text: "你将无法恢复该文件，你的行为将被记录！",
-            type: "warning",
-            showCancelButton: true,
+            icon: "warning",
+            buttons: true,
             confirmButtonColor: "#DD6B55",
             confirmButtonText: "确定删除！",
-            closeOnConfirm: false
-        }.then(
-        function(){
-            $.ajax({
-                url: '/delete_image',
-                type: 'POST',
-                cache: false,
-                data: {'id':detail_id
-                },
-                processData: false,
-                contentType: false,
-                success:function(res){
-                    swal("删除！", "文件已经被删除。", "success");
-                }
-            }).done(function(res) {
-            }).fail(function(res) {
-            });
-        })
-    );
+            dangerMode: true,
+        }).then(function(willDelete){
+            if(willDelete) {
+                $.ajax({
+                    url: '/deleteImg',
+                    type: 'POST',
+                    data: {
+                        'id': detail_id
+                    },
+                    success: function (res) {
+                        swal("删除！", "文件已经被删除。", "success");
+                    }
+                }).done(function (res) {
+                }).fail(function (res) {
+                });
+            }else{
+                swal("已取消");
+            }
+        });
 });
